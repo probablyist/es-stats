0a9d3bee75315990d0d821a3fd62ce30f8a65088
Paul <paul.probst@gmail.com>
2022-06-10 20:19:38 -0400

    Add method to load CSV to database

--- app/models/price_data.rb
+++ app/models/price_data.rb
@@ -1,2 +1,16 @@
 class PriceData < ApplicationRecord
+  def self.load_csv_to_db
+    url = URI(ENV["es_data_sheet"])
+    body = url.read
+
+    CSV.parse(body, col_sep: ",", headers: true) do |row|
+      PriceData.create({
+        date_time: row["Date Time"].to_datetime,
+        open: row["Open"].to_f,
+        high: row["High"].to_f,
+        low: row["Low"].to_f,
+        close: row["Close"].to_f
+      })
+    end
+  end
 end
