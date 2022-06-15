641ccc1182526dc769cd3b0809e072cfc6c50c07
Paul <paul.probst@gmail.com>
2022-06-14 21:51:05 -0400

    Add statistic methods

--- app/models/stats.rb
+++ app/models/stats.rb
@@ -1,7 +1,140 @@
 class Stats < ApplicationRecord
-has_many :price_data
-  # Adds All unique trading days from PriceData table
+  has_many :price_data
 
+  def self.period_hash
+    period_hash = {
+      A: 0,
+      B: 0,
+      C: 0,
+      D: 0,
+      E: 0,
+      F: 0,
+      G: 0,
+      H: 0,
+      I: 0,
+      J: 0,
+      K: 0,
+      L: 0,
+      M: 0,
+      N: 0,
+      O: 0
+    }
+  end
+
+  def self.count_breach_fh
+      high = period_hash.except(:A, :B)
+      low = period_hash.except(:A, :B)
+      either = period_hash.except(:A, :B)
+      both = 0
+      none = 0
+
+    Stats.all.each do |s|
+      if s.breach_fhh.present? && s.breach_fhl.present?
+        both += 1
+        if s.breach_fhh < s.breach_fhl
+          either[s.breach_fhh.to_sym] += 1
+        else
+          either[s.breach_fhl.to_sym] += 1
+        end
+      elsif s.breach_fhh.present?
+        high[s.breach_fhh.to_sym] += 1
+        either[s.breach_fhh.to_sym] += 1
+      elsif s.breach_fhl.present?
+        low[s.breach_fhl.to_sym] += 1
+        either[s.breach_fhl.to_sym] += 1
+      else
+        none += 1
+      end
+    end
+
+    breach_on = {
+      high: high,
+      low: low,
+      either: either,
+      both: both,
+      none: none
+    }
+  end
+
+  def self.count_breach_on
+    high = period_hash
+    low = period_hash
+    either = period_hash
+    both = 0
+    none = 0
+
+  Stats.all.each do |s|
+    sh = s.breach_onh
+    sl = s.breach_onl
+    if sh.present? && sl.present?
+        both += 1
+      high[sh.to_sym] += 1
+      low[sl.to_sym] += 1
+      if sh < sl
+        either[sh.to_sym] += 1
+      else
+        either[sl.to_sym] += 1
+      end
+    elsif sh.present?
+      high[sh.to_sym] += 1
+      either[sh.to_sym] += 1
+    elsif sl.present?
+      low[sl.to_sym] += 1
+      either[sl.to_sym] += 1
+    else
+      none += 1
+    end
+  end
+
+  breach_on = {
+    high: high,
+    low: low,
+    either: either,
+    both: both,
+    none: none
+  }
+end
+
+def self.count_breach_a
+  either = 0
+  both = 0
+  high = []
+  low = []
+
+  Stats.all.each do |s|
+    if s.breach_ah.present? && s.breach_al.present?
+      either += 1
+      both += 1
+      high << s.breach_ah.to_f
+      low << s.breach_al.to_f
+    elsif s.breach_ah.present?
+      either += 1
+      high << s.breach_ah.to_f
+    elsif s.breach_al.present?
+      either += 1
+      low << s.breach_al.to_f
+    end
+
+  end
+
+  high = { count: high.size, avg: mean_ticks(high) }
+  low = { count: low.size, avg: mean_ticks(low) }
+
+  breach_a = {
+    either: either,
+    both: both,
+    high: high,
+    low: low
+  }
+end
+
+def self.mean_ticks(array)
+  mean = array.sum(0.0) / array.size
+  mean = (mean * 4).round / 4.0
+end
+
+
+  ##### UPDATE FIELDS #####
   def self.update_breaches
     add_breach_onh
     add_breach_onl
@@ -11,6 +144,7 @@ has_many :price_data
     add_breach_a_low
   end
 
+  # Adds All unique trading days from PriceData table
   def self.add_trading_days_to_table
     PriceData.all.distinct.pluck(:trading_day).each do |d|
       Stats.find_or_create_by(trading_day: d)
@@ -72,11 +206,11 @@ has_many :price_data
   # Adds B period breach of A period high
   def self.add_breach_a_high
     Stats.all.each do |s|
-      a = PriceData.a_period.where(trading_day: s.trading_day).pluck(:high)
-      b = PriceData.b_period.where(trading_day: s.trading_day).pluck(:high)
+      a = PriceData.a_period.where(trading_day: s.trading_day).pluck(:high).first
+      b = PriceData.b_period.where(trading_day: s.trading_day).pluck(:high).first
 
-      if b[0] > a[0]
-        s.update!(breach_ah: "B")
+      if b > a
+        s.update!(breach_ah: b-a)
       end
     end
   end
@@ -84,11 +218,11 @@ has_many :price_data
   # Adds B period breach of A period low
   def self.add_breach_a_low
     Stats.all.each do |s|
-      a = PriceData.a_period.where(trading_day: s.trading_day).pluck(:low)
-      b = PriceData.b_period.where(trading_day: s.trading_day).pluck(:low)
+      a = PriceData.a_period.where(trading_day: s.trading_day).pluck(:low).first
+      b = PriceData.b_period.where(trading_day: s.trading_day).pluck(:low).first
 
-      if b[0] < a[0]
-        s.update!(breach_al: "B")
+      if b < a
+        s.update!(breach_al: a-b)
       end
     end
   end
