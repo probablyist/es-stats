e8b38afa1ee407dd650dbd30f1e508a1ab2e6f82
Paul <paul.probst@gmail.com>
2022-06-26 17:07:17 -0400

    Add table styling

--- app/lib/pull_stats.rb
+++ app/lib/pull_stats.rb
@@ -1,16 +1,14 @@
 module PullStats
 
 
-  def fhhl_stats
+  def fhhl_stats_hash
     fhhl_stats = {
       period_stats: fhhl_period_stats,
       day: fhhl_day_stats
     }
   end
 
-  def fhhl_period_stats
-    total = Stats.all.size
-
+  def fhhl_period_stats(total)
     period_stats = {
       fhh_period: hash_to_percent(self.count_breach_fhh_to_hash, total),
       fhl_period: hash_to_percent(self.count_breach_fhl_to_hash, total),
@@ -19,41 +17,12 @@ module PullStats
     }
   end
 
-  def fhhl_day_stats
-    total = Stats.all.size
-
+  def fhhl_day_stats(total)
     day = {
       fhhl_both: as_percent_of(self.count_breach_both_sc, total),
-      fhhl_either: as_percent_of(self.count_breach_either_sc, total),
+      fhhl_either: as_percent_of(self.count_breach_either_sc, total)
     }
   end
-
-
-  # def fill_breach_fhhl
-  #   period = empty_stat_hash_periods
-  #   day = empty_stat_hash_days
-
-  #   period[:high][:B] = 1
-  #   period[:high][:C] = 10
-
-  #   accum = add_accum_stats_hash(period)
-
-  #   return day, period, accum
-  # end
-
-  # def count_breach
-  #   hash = empty_periods_hash
-  #   hash.each do |k, v|
-  #     hash[k] = Stats.where(breach_fhl: k.to_s).size
-  #   end
-  # end
-
-  # def count_breach
-  #   hash = empty_periods_hash
-  #   hash.each do |k, v|
-  #     hash[k] = get_breach_fhl(k).size
-  #   end
-  # end
 
   def count_breach_fhh_to_hash
     hash = empty_periods_hash.except(:A, :B)
@@ -83,8 +52,6 @@ module PullStats
   end
 
   def hash_to_percent(hash, total)
-    p hash
-    p total
     hash.each do |k, v|
       hash[k] = as_percent_of(v, total) #unless hash[k].blank?
     end
